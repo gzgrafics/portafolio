5992a3a26443d3bdcef098ee1d107db489debed8
gzgrafics <gz.desingrafic@gmail.com>
2019-02-06 23:29:02 -0300

    subir nav header desk and mobile

--- js/main.js
+++ js/main.js
@@ -12,8 +12,31 @@ $(document).ready(function() {
     }, false);
     /*hamburguer*/
 
+
+    /*menu-responsive*/
     $('.menu-burguer').click(function(){
         $('.gz-menu').toggleClass('active');
+        $('.gz-menu a').toggleClass('fadeInRight');
+        $('.overlay').toggleClass('active');
     });
 
+    $('.overlay').click(function(){
+        $(this).removeClass('active');
+        $('.gz-menu').removeClass('active');
+        $('.menu-burguer').removeClass('open');
+        $('.gz-menu a').removeClass('fadeInRight');
+    });
+
+    $('.gz-menu a').click(function(){
+        $('.gz-menu').removeClass('active');
+        $('.overlay').removeClass('active');
+        $('.menu-burguer').removeClass('open');
+        $('.gz-menu a').removeClass('fadeInRight');
+    });
+    /*menu-responsive*/
+
+});
+
+$(window).load(function(){
+    $('.gz-nav').addClass('down-load');
 });
